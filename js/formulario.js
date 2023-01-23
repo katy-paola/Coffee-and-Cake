f0cca262c2522d57a4abdc34b7d49ba0fd06d04a
Katy <kbarbozab@unicartagena.edu.co>
2023-01-23 15:49:45 -0500

    validando inputs del form con js

--- js/formulario.js
+++ js/formulario.js
@@ -7,6 +7,8 @@
         formulario: document.formulario_contacto, //seleccionamos el formulario mediante su atributo name, que en este
                                                   //caso es formulario_contacto
         elementos: document.formulario_contacto.elements, //seleccionamos todos los elementos del formulario
+        error: null, //variable para determinar si hay un error o no
+        textoError: null //texto que se mostrará en el error
     }
     var metFormulario = {
         //metodos
@@ -18,6 +20,9 @@
                     propFormulario.elementos[i].addEventListener('blur', metFormulario.blurInput);
                 }
             }
+
+            //cuando el formulario se envíe, se ejecutará la función validarInputs
+            propFormulario.formulario.addEventListener('submit', metFormulario.validarInputs);
         },
         focusInput: function(){
             //el this hace referencia al elemento que está en foco
@@ -34,6 +39,29 @@
             //si el elemento que está en foco no tiene valor, entonces le quitamos la clase active
             if(this.value == ''){
                 this.parentElement.children[1].className = 'label';
+            }
+        },
+        validarInputs: function(e){
+            //recorremos todos los elementos del formulario
+            for(var i = 0; i < propFormulario.elementos.length; i++){
+                
+                //si el elemento está vacío
+                if(propFormulario.elementos[i].value == ''){
+                    e.preventDefault(); //evitamos que el formulario se envíe
+                    propFormulario.error = document.createElement('p'); //creamos un elemento p
+                    propFormulario.textoError = document.createTextNode('El campo ' + propFormulario.elementos[i].name + ' es obligatorio');
+                    propFormulario.error.appendChild(propFormulario.textoError); //agregamos el texto al elemento p
+                    propFormulario.error.className = 'error'; //agregamos la clase error al elemento p
+                    propFormulario.elementos[i].parentElement.appendChild(propFormulario.error); //agregamos el elemento p al elemento padre del elemento que está vacío
+                }
+                else{
+                    //si el elemento no está vacío, entonces revisamos si tiene la clase error, es decir, si tiene
+                    //un tercer hijo que corresponde al elemento p con la clase error
+                    if(propFormulario.elementos[i].parentElement.children.length >= 3){
+                        //si tiene la clase error, entonces la eliminamos
+                        propFormulario.elementos[i].parentElement.removeChild(propFormulario.elementos[i].parentElement.lastChild);
+                    }
+                }
             }
         }
     }
